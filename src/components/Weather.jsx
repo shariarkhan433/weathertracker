ad5d282ead6cf716fefdf6cf8690099ec44f325d
kshariare <kshariare@gmail.com>
2024-08-23 20:55:16 +0000

    Basic structures of the App is made.

---
+++ src/components/Weather.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Weather.css'
+
+import search_icon from '../assets/search.png';
+import clear_icon from '../assets/clear.png';
+import cloud_icon from '../assets/cloud.png';
+import drizzle_icon from '../assets/drizzle.png';
+import humidity_icon from '../assets/humidity.png';
+import wind_icon from '../assets/wind.png';
+import snow_icon from '../assets/snow.png';
+import rain_icon from '../assets/rain.png';
+
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+        <div className="search-bar">
+            <input type="text" placeholder='Search...'/>
+            <img src={search_icon} alt="" />
+        </div>
+            <img src={clear_icon} alt="" className='weather-icon'/>
+           <div className='weather-details'>
+            
+            <p>26°C</p>
+            <p>Dhaka</p>
+           </div>
+    </div>
+  )
+}
+
+export default Weather
